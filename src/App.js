5acd861c24a978914b6d14f6a55bd4500de0fbf1
ilikeu7246 <jsunee7246@gmail.com>
2019-10-24 19:06:16 +0900

    Update code to 'CSSModule' version. #2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import SassComponent from "./SassComponent";
+import CSSModule from './CSSModule';
+
 
 
 function App() {
   return (
-    <SassComponent />
+    <CSSModule />
   );
 }
 
